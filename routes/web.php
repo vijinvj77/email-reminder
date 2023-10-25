96f21926cb9c15457c391ecbe6c651c23f00e093
VIJIN V J <vijinvj77@gmail.com>
2023-10-25 21:58:28 +0530

    Authentication using bootstrap auth scaffolding completed

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Email Verification Routes
+Route::get('/email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('/email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('/email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
